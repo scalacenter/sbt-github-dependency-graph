962695ee23386ed2d74ffdf0a10d81d73efac6a5
Adrien Piquerez <adrien.piquerez@gmail.com>
2022-06-22 14:27:21 +0200

    Add support for selection of projects and scala versions

--- src/test/scala/ch/epfl/scala/JsonProtocolTests.scala
+++ src/test/scala/ch/epfl/scala/JsonProtocolTests.scala
@@ -1,6 +1,5 @@
-package ch.epfl.scala.githubapi
+package ch.epfl.scala
 
-import ch.epfl.scala.githubapi.JsonProtocol._
 import munit.FunSuite
 import sjsonnew.shaded.scalajson.ast.unsafe.JField
 import sjsonnew.shaded.scalajson.ast.unsafe.JNumber
@@ -8,9 +7,11 @@ import sjsonnew.shaded.scalajson.ast.unsafe.JObject
 import sjsonnew.shaded.scalajson.ast.unsafe.JString
 import sjsonnew.shaded.scalajson.ast.unsafe.JValue
 import sjsonnew.support.scalajson.unsafe.Converter
+import sjsonnew.support.scalajson.unsafe.Parser
 
 class JsonProtocolTests extends FunSuite {
   test("encode metadata") {
+    import ch.epfl.scala.githubapi.JsonProtocol._
     val metadata = Map("key1" -> JString("value1"), "key2" -> JNumber(1))
     val obtained = Converter.toJson(metadata).get
     val expected = JObject(JField("key1", JString("value1")), JField("key2", JNumber(1)))
@@ -18,9 +19,18 @@ class JsonProtocolTests extends FunSuite {
   }
 
   test("decode metadata") {
+    import ch.epfl.scala.githubapi.JsonProtocol._
     val metadata = JObject(JField("key1", JString("value1")), JField("key2", JNumber(1)))
     val obtained = Converter.fromJson[Map[String, JValue]](metadata).get
     val expected = Map("key1" -> JString("value1"), "key2" -> JNumber(1))
+    assertEquals(obtained, expected)
+  }
+
+  test("decode empty input") {
+    import ch.epfl.scala.JsonProtocol._
+    val raw = Parser.parseUnsafe("{}")
+    val obtained = Converter.fromJson[SubmitInput](raw).get
+    val expected = SubmitInput(Vector.empty, Vector.empty)
     assertEquals(obtained, expected)
   }
 }
